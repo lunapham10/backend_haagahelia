60beac5cf2f0e228d67df8348c6fae57ff47b976
bhu042@myy.haaga-helia.fi <kieuoanh23111993@gmail.com>
2026-02-01 11:42:29 +0200

    list

--- bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ bookstore/src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -3,15 +3,39 @@ package fi.haagahelia.bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+
+import fi.haagahelia.bookstore.domain.Book;
+import fi.haagahelia.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+
+    private BookRepository repository;
+    public BookController(BookRepository repository){
+        this.repository = repository;
+    }
+
     @RequestMapping(
-        value = {"/index"},
+        value = {"/booklist"},
         method = {RequestMethod.GET}
     )
-    public String bookStoreProject(){
-        // do something
-        return "index";
+    public String bookList(Model model){
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
+    
+    @RequestMapping(
+        value = "/addbook"
+    )
+    public String addBook(Model model){
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+
+    // NEED TO MAP ONE GET ENDPOINT HERE LIKE /LISTBOOKS
+
+
+
 }
